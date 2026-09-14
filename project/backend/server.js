57c9b8e969bc92865d4529aa97131cd9bea991a6
Asim405 <asimsultan405@gmail.com>
2026-09-14 16:33:59 +0500

    server.js checking

--- project/backend/server.js
+++ project/backend/server.js
@@ -49,6 +49,7 @@ app.use((err, req, res, next) => {
 // Start server
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Health check available at http://localhost:${PORT}/api/health`);
 });
 
 module.exports = app;
